7021fd0d00c0e6638c4cb369f8bd7e44fa7fdbed
sidahmed <ahorob57@gmail.com>
2021-05-04 10:29:18 +0100

    comment the api

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -2,7 +2,7 @@ import { server } from '../../../config/index'
 import Link from 'next/link'
 import Meta from '../../../components/Meta'
 
-export default function article({ article }) {
+export default function article({ article = { title: 'cool', body: 'cool body' } }) {
     return (
         <>
             <Meta title={article.title} />
@@ -13,26 +13,26 @@ export default function article({ article }) {
     )
 }
 
-export const getStaticProps = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6') //fetch(`${server}/api/articles/${params.id}`)
-    const article = await response.json()
-    return {
-        props: {
-            article
-        },
-        revalidate: 900000
-    }
-}
+// export const getStaticProps = async ({ params }) => {
+//     const response = await fetch(`${server}/api/articles/${params.id}`)
+//     const article = await response.json()
+//     return {
+//         props: {
+//             article
+//         },
+//         revalidate: 900000
+//     }
+// }
 
-export const getStaticPaths = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6') //fetch(`${server}/api/articles/`)
-    const articles = await response.json()
-    const ids = articles.map((artcile) => artcile.id)
-    const paths = ids.map((id) => ({
-        params: { id: id.toString() }
-    }))
-    return {
-        paths,
-        fallback: false
-    }
-}
+// export const getStaticPaths = async () => {
+//     const response = await fetch(`${server}/api/articles/`)
+//     const articles = await response.json()
+//     const ids = articles.map((artcile) => artcile.id)
+//     const paths = ids.map((id) => ({
+//         params: { id: id.toString() }
+//     }))
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
